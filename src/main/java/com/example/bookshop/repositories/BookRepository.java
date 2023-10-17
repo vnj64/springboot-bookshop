b2a0ea8e382919c6d297c6663a28f9d1fab5606f
vnJ <kamil_khabibullin@bk.ru>
2023-10-17 22:11:11 +0300

    sorting method aded

--- src/main/java/com/example/bookshop/repositories/BookRepository.java
+++ src/main/java/com/example/bookshop/repositories/BookRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByAuthor(Author author);
+    List<Book> findByBookNameContainingIgnoreCase(String bookName);
 }
