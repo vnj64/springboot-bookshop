23840346599a52c9c2793e8dca94d606f70fb851
vnJ <kamil_khabibullin@bk.ru>
2023-10-19 01:41:15 +0300

    update author method

--- src/main/java/com/example/bookshop/controllers/AuthorController.java
+++ src/main/java/com/example/bookshop/controllers/AuthorController.java
@@ -1,10 +1,12 @@
 package com.example.bookshop.controllers;
 
+import com.example.bookshop.exception.AuthorNotFoundException;
 import com.example.bookshop.models.Author;
 import com.example.bookshop.repositories.AuthorRepository;
 import com.example.bookshop.services.AuthorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -31,5 +33,15 @@ public class AuthorController {
     public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
         Author createdAuthor = authorService.createAuthor(author);
         return ResponseEntity.ok(createdAuthor);
+    }
+
+    @PutMapping(value = "/update/{authorId}")
+    public ResponseEntity<?> updateAuthor(@PathVariable Long authorId, @RequestBody Author updatedAuthor) {
+        try {
+            Author updatedAuthorEntity = authorService.updateAuthor(updatedAuthor, authorId);
+            return ResponseEntity.ok(updatedAuthorEntity);
+        } catch (AuthorNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Автор с ID: " + authorId + " не найден.");
+        }
     }
 }
