b2995db4d20ace943362d88bcb49666b6bae924c
vnJ <kamil_khabibullin@bk.ru>
2023-10-19 01:42:16 +0300

    update author service added

--- src/main/java/com/example/bookshop/services/AuthorService.java
+++ src/main/java/com/example/bookshop/services/AuthorService.java
@@ -1,11 +1,13 @@
 package com.example.bookshop.services;
 
+import com.example.bookshop.exception.AuthorNotFoundException;
 import com.example.bookshop.models.Author;
 import com.example.bookshop.repositories.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +24,20 @@ public class AuthorService {
 
     public Author getAuthorById(Long authorId) {
         return authorRepository.findById(authorId).orElse(null);
+    }
+
+    public Author updateAuthor(Author updatedAuthor, Long authorId) {
+        Author existingAuthor = authorRepository.findById(authorId).orElse(null);
+        if (existingAuthor == null) {
+            throw new AuthorNotFoundException("Автор с ID: " + updatedAuthor.getId() + " не найден.");
+        }
+        existingAuthor.setFirstName(updatedAuthor.getFirstName());
+        existingAuthor.setPatronymic(updatedAuthor.getPatronymic());
+        existingAuthor.setSurname(updatedAuthor.getSurname());
+        existingAuthor.setBirthDate(updatedAuthor.getBirthDate());
+        existingAuthor.setBiography(updatedAuthor.getBiography());
+
+
+        return authorRepository.save(updatedAuthor);
     }
 }
